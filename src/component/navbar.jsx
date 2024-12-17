d9d413cb1deb9c61b9fd0a2c0296d0e89ce71807
ElioPeraza <perazaelio@gmail.com>
2024-12-17 15:29:12 -0300

    lastchange

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -9,7 +9,7 @@ export const Navbar = () => {
     return (
         <nav className="navbar navbar-light bg-light mb-3">
             <Link to="/">
-                <span className="navbar-brand mb-0 h1">Star Wars Blog</span>
+                <span className="navbar-brand mb-0 h1">STAR WAR BLOG</span>
             </Link>
             <div className="ml-auto">
                 <Link to="/favorites">
